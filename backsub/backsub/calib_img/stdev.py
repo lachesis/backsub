6e3d1ff7d83a44dd246b9d4276822ebf59ce5a7c
Eric Swanson <eswanson@alloscomp.com>
2012-08-16 02:19:56 -0400

    changed py-stdev algo

--- backsub/backsub/calib_img/stdev.py
+++ backsub/backsub/calib_img/stdev.py
@@ -11,26 +11,17 @@ for x in xrange(0,640):
         stdevd[(x,y)] = [0,0,0]
 
 FRAMES = 20
-pas = []
 for idx in xrange(0,FRAMES):
     print "Loading image {0}".format(idx+100)
     i = Image.open('output0{0}.png'.format(idx+100))
     pa = i.load()
-    pas.append(pa)
 
-for pa in pas:
     for x in xrange(0,640):
         for y in xrange(0,480):
             p = pa[x,y]
             for chan in xrange(3):
-                avgd[(x,y)][chan] += (p[chan] / FRAMES) 
-
-for pa in pas:
-    for x in xrange(0,640):
-        for y in xrange(0,480):
-            p = pa[x,y]
-            for chan in xrange(3):
-                sumsqd[(x,y)][chan] += (p[chan] ** 2) / FRAMES
+                avgd[(x,y)][chan] += (p[chan]/256.0 / FRAMES) 
+                sumsqd[(x,y)][chan] += (p[chan]/256.0 ** 2) / FRAMES
 
 avgi = i.copy()         
 pa = avgi.load()
@@ -39,6 +30,6 @@ for x in xrange(0,640):
         stdev = [0,0,0]
         for chan in xrange(3):
             stdev[chan] = math.sqrt(sumsqd[(x,y)][chan] - (avgd[(x,y)][chan]) ** 2)
-        pa[x,y] = tuple([int(q) for q in stdev])
+        pa[x,y] = tuple([int(q*256.0) for q in stdev])
 avgi.save('stdev.png')
 
